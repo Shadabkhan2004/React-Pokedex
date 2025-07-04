ebbd22832a5b564b78933dee3b52d53cd5015ed0
Shadabkhan2004 <iamshadabkhan0@gmail.com>
2025-07-04 09:44:08 +0530

    uploaded files

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import  Header  from './components/Header'
+import SideNav from './components/SideNav'
+import  PokeCard  from './components/PokeCard'
+function App() {
+
+  const [selectedPokemon, setSelectedPokemon] = useState(0)
+  const [showSideMenu, setShowSideMenu] = useState(true)
+
+
+  function handleToggleMenu(){
+    setShowSideMenu(prev => !prev)
+  }
+
+  function handleCloseMenu(){
+    setShowSideMenu(false)
+  }
+  return (
+    <>
+      <Header handleToggleMenu={handleToggleMenu}/>
+      <SideNav selectedPokemon={selectedPokemon} 
+      setSelectedPokemon={setSelectedPokemon} handleCloseMenu={handleCloseMenu}
+      showSideMenu={showSideMenu}
+      />
+      <PokeCard selectedPokemon={selectedPokemon}/>
+
+    </>
+  )
+}
+
+export default App
